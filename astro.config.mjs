172d18fedc655bfce826942865d5788920e932b9
mcgalleg <michael.c.gallegos@gmail.com>
2025-02-15 20:56:12 -0700

    Enable site-wide prefetching and add Astro view transitions support

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,6 +10,9 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://rositas.biz',
+  prefetch: {
+    prefetchAll: true
+  },
   integrations: [
     tailwind(), 
     svelte(),
